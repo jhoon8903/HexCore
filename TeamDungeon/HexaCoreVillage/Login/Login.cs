bfd1f6489f1c9526e3327074c4bda43dd45f39b3
I'm Developer? <59020441+iamdeveloperz@users.noreply.github.com>
2023-11-16 16:32:37 +0900

    [BugTest] : using 지시문

--- TeamDungeon/HexaCoreVillage/Login/Login.cs
+++ TeamDungeon/HexaCoreVillage/Login/Login.cs
@@ -2,7 +2,17 @@
 using HexaCoreVillage.Framework;
 namespace HexaCoreVillage.Login;
 
-public class Login
+public class Login : Scene
 {
-    
+    public override SCENE_NAME SceneName => throw new NotImplementedException();
+
+    public override void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ TeamDungeon/HexaCoreVillage/Manager/Manager_Scene.cs
@@ -0,0 +1,29 @@
+﻿
+using HexaCoreVillage.Framework;
+using HexaCoreVillage.Login;
+using Login = HexaCoreVillage.Login.Login;
+
+namespace HexaCoreVillage.Manager;
+
+/// <summary>
+/// # Scene을 관리하는 매니저 클래스
+/// ## Scene by Scene : 씬을 이동 시키는 매개 클래스
+/// </summary>
+public class Manager_Scene
+{
+    #region Member Variables
+    private Scene? _currentScene = null;
+    #endregion
+
+    #region Main Methods
+    public void LoadScene(SCENE_NAME sceneKey)
+    {
+        _currentScene = sceneKey switch
+        {
+            SCENE_NAME.TITLE => new Title(),
+            SCENE_NAME.LOGIN => new Login(),
+
+        };
+    }
+    #endregion
+}
