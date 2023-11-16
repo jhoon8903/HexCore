b1ba687c5bf87982f191801b7cd84cd484d5bb95
I'm Developer? <59020441+iamdeveloperz@users.noreply.github.com>
2023-11-16 15:23:54 +0900

    [➕ ADD] - Framework, (SceneBase, SingletonBase)
    # 기본 프레임워크 작성
      - Scene.cs (abstract class)
      - Singleton.cs (singleton base super class)
    
      - Title.cs -> Main() 함수 옮김
      - MainProcedure.cs 추가 -> Main()
      - 예제파일) Title.cs 및 MainProocedure.cs 확인

---
+++ TeamDungeon/HexaCoreVillage/Framework/Scene.cs
@@ -0,0 +1,26 @@
+﻿
+namespace HexaCoreVillage.Framework;
+
+
+/// <summary>
+/// # Scene에 베이스가 되는 추상 클래스
+/// </summary>
+public abstract class Scene
+{
+    public abstract SCENE_NAME SceneName { get; }
+
+    public abstract void Start();
+    public abstract void Update();
+}
+
+// 추 후 Utility 같은 정적 클래스로 뺄 예정
+public enum SCENE_NAME
+{
+    TITLE,
+    LOGIN,
+    LOBBY,
+    STATUS,
+    STORE,
+    BATTLE,
+    REWARD
+}
