9a3dfb20e974e9797c2d9eccb69b4d270e5998bc
shstcs <shstcs@naver.com>
2023-11-16 15:25:30 +0900

    [Feature] Start / SetNewCharacter
    -새로 시작/불러오기 나눔.
    -저장하고 불러오는 메소드 생성.
    -키보드 방향키 입력으로 이동하도록 구현.

--- TeamDungeon/HexaCoreVillage/Login/Title.cs
+++ TeamDungeon/HexaCoreVillage/Login/Title.cs
@@ -1,9 +1,12 @@
+using HexaCoreVillage.Utility;
+
 namespace HexaCoreVillage.Login;
 
 public static class Title
 {
     public static void Main()
     {
-
+        Login.LoginScene();
+        Login.SaveData();
     }
 }
