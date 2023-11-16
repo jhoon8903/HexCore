9f698b3b79fe7ebd72fd84ce89cb02d3ffc83f56
Daniel Lee <jhoon8903@gmail.com>
2023-11-16 12:57:06 +0900

    [🛠️FIx] -  Character and Item Class Fix
    - 해당 스크립트 병합 문제로 케릭터 및 아이템 클래스 재병합 진행

---
+++ TeamDungeon/TeamDungeon/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamDungeon
+{
+    internal class Player
+    {
+        string ID {  get; set; }
+        string NickName { get; set; }
+        Job job {  get; set; }
+        int Level { get; set; }
+        int Exp {  get; set; }
+        int TypingSpeed {  get; set; }
+        int C {  get; set; }
+        int BonusDmg { get; set; }
+        int BounsDef {  get; set; }
+        int HP {  get; set; }
+        int CurrentHp {  get; set; }
+        int Mental {  get; set; }
+        int CurrentMental {  get; set; }
+        int Gold {  get; set; }
+        List<InvenItem> Inventory = new List<InvenItem>();
+
+        public Player()
+        {
+            ID = "enos";
+            NickName = "주니어개발자";
+            job = Job.AI;
+            Level = 1;
+            Exp = 0;
+            TypingSpeed = 10;
+            C = 10;
+            BonusDmg = 10;
+            BounsDef = 10;
+            HP = 100;
+            CurrentHp = 100;
+            Mental = 10;
+            Gold = 1000;
+        }
+    }   
+
+    enum Job
+    {
+        Unity,
+        Unreal,
+        AI,
+        PM,
+        QA,
+        Neo
+    }
+}
