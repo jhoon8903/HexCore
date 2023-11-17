1d900d89e2d12f9404183195e31dbd2d976ef4fa
Daniel Lee <jhoon8903@gmail.com>
2023-11-17 19:24:07 +0900

    [💫 Feature] - Console Size Fix Renderer
    - 콘솔 사이즈 픽서 유틸리티 생성
    - Utility 폴더의 ConsoleSizeUtility Class
    - ConsoleSizeUtility.RedrawBorder(); 반복문 마지막에 삽입 시 랜더링 후 콘솔 사이즈 배경을 그림
    - 상단 첫줄과 하단 마지막 줄 그리고 좌측 첫쨰줄은 비워 주셔야 합니다. 개행문자 \t 로 통일하는게 좋을 것 같습니다.

--- TeamDungeon/HexaCoreVillage/Dungeon/Battle.cs
+++ TeamDungeon/HexaCoreVillage/Dungeon/Battle.cs
@@ -4,37 +4,47 @@ using System.Text;
 
 namespace HexaCoreVillage.Dungeon
 {
+    public static class Debugging
+    {
+        public enum SolutionTypes{ ModifyVariables, FixTypos, ModifyScope, EditPath, CheckMemory }
+    }
     public class Battle  : Scene
     {
         public override SCENE_NAME SceneName => SCENE_NAME.BATTLE;
         private static Player? Player { get; set; }
-        private static readonly Random Percentage = new Random();
-        public static int BugPercentage = 15;
         private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
         private static readonly string BugFilePath = Path.GetFullPath(Path.Combine(BaseDirectory, "..", "..", "..", "..", "..", "TeamDungeon", "HexaCoreVillage", "Utility", "BugList.json"));
         private static List<Bug>? _bugList = new List<Bug>();
         private static List<Bug>? _selectedBugs;
-        private const int BugsCount = 4;
         private static readonly Random Random = new Random();
 
+        # region Start
+        /// <summary>
+        ///  Player 데이터 파싱
+        /// </summary>
         public override void Start()
-        { 
+        {
+            Player = new Player();
+            Player.BugPercentage += 15;
             ListOfBug();
         }
+        #endregion
 
+        #region Loop Logic
         public override void Update()
         {
             Compiling();
-            Bug selectBug = DebuggingList();
-            if (selectBug != null)
+            List<Bug> selectBugs = DebuggingList();
+            if (selectBugs != null)
             {
-                Debugging(selectBug);               
+                Data.BattleSuccess = Debugging(selectBugs);
             }
             else
             {
                 Escape();
             }
         }
+        #endregion
 
         # region BugList
         /// <summary>
@@ -59,100 +69,133 @@ namespace HexaCoreVillage.Dungeon
         /// </summary>
         private static void Compiling()
         {
+            CursorVisible = false;
             while (true)
             {
                 // "....."이 10초 단위로 Loop 하면서 초기화 진행
                 for (int i = 0; i < 10; i++)
                 {
                     Clear();
-                    WriteLine("[Running To Project]\n");
+                    WriteLine("\n\n\t[Running To Project]\n");
                     // 초당 1개씩 Dot을 추가
-                    Write("Compiling" + new string('.', i));
+                    Write("\tCompiling" + new string('.', i));
                     
-                    if (Percentage.Next(0,100) <=  BugPercentage)
+                    if (Random.Next(0,100) <=  Player.BugPercentage)
                     {
                         FoundBugs();
                         return;
-                    }
+                    }    
+                    ConsoleSizeUtility.RedrawBorder();
                     Thread.Sleep(1000);
                 }
             }
         }
 
         /// <summary>
-        ///  Json에서 받아온 버그 리스트 중 랜덤하게 1 ~ 3 개의 버그 리스트 제공
+        ///  Json에서 받아온 버그 리스트 중 랜덤하게  버그 리스트 제공
+        ///  버그의 난이도에 따라서 5이상 5 이하로 구분하여 진행
         /// </summary>
         private static void FoundBugs()
         {
+            int bugsCount = 0;
             _selectedBugs = new List<Bug>();
-            int count = Random.Next(1, BugsCount);
+            bugsCount = Player.Level switch
+            {
+                1 => Random.Next(1, 4),
+                2 =>  Random.Next(1, 5),
+                3 =>  Random.Next(2, 5),
+                4 =>  Random.Next(3, 6),
+                5 =>  Random.Next(4, 7),
+                _ => 8
+            };
+
+            int count = Random.Next(1, bugsCount + 1);
+
             for (int i = 0; i < count; i++)
             {
-                if (_bugList == null)
+                if (_bugList == null || _bugList.Count == 0)
                 {
                     continue;
                 }
-                int index = Random.Next(_bugList.Count);
-                _selectedBugs.Add(_bugList[index]);
+
+                Bug selectedBug;
+                do
+                {
+                    int index = Random.Next(_bugList.Count);
+                    selectedBug = _bugList[index];
+                }
+                while (Player.Level <= 5 && selectedBug.BugDifficulty > 5);
+
+                _selectedBugs.Add(selectedBug);
             }
         }
 
         /// <summary>
-        ///  Console에 FoundBugs 중에서 고른 Bug 리스트를 출력
+        ///  Console에 FoundBugs 중에서 고른 Bug 리스트의 첫 번재 버그를 출력
         /// </summary>
-        /// <returns>선택한 Bug를 반환</returns>
-        private static Bug DebuggingList()
+        /// <returns>선택한 Bug 리스트를 반환</returns>
+        private static List<Bug> DebuggingList()
         {
             CursorVisible = false;
-            int selectedIndex = 0;
-            int totalOption = _selectedBugs.Count + 1;
+            Bug selectedBug = _selectedBugs[Random.Next(_selectedBugs.Count)];
+            List<Bug> selectedBugs = new List<Bug>(); 
+            selectedBugs.Add(selectedBug);
+            selectedBugs.AddRange(_selectedBugs.Where(b => b != selectedBug));
+            int choiceOption = 0;
+
             while (true)
             {
                 Clear();
-                WriteLine("[디버깅]\n");
-                WriteLine("[확인 된 버그 목록]\n"); 
-                WriteLine("[디버깅을 시작할 버그를 선택해주세요]\n");
+                WriteLine("\n\n\t[디버깅 - 버그 확인]\n");
+                WriteLine($"\t[{Player.NickName}의 상태]"); 
+                WriteLine($"\t레벨 : {Player.Level}");
+                WriteLine($"\t체력 : {Player.CurrentHp} / {Player.HP}"); 
+                WriteLine($"\t디버깅 능력 : {Player.TypingSpeed}");
+                WriteLine($"\t버그대응 능력 : {Player.C}\n"); 
+                Write("\t");
+                BackgroundColor = ConsoleColor.Red;
+                ForegroundColor = ConsoleColor.Black;
+                Write($"[Compile Error!!]\n");
+                ResetColor();
+                Write("\t");
+                BackgroundColor = ConsoleColor.Red;
+                ForegroundColor = ConsoleColor.Black;
+                Write($"[{selectedBugs.Count}개의 버그가 확인 되었습니다! - 확인 된 버그는 다음과 같습니다.]\n\n");
+                ResetColor();
 
-                if (_selectedBugs != null)
+                if (choiceOption == 0)
                 {
-                    for (int i = 0; i < _selectedBugs.Count; i++)
-                    {
-                        if (i == selectedIndex)
-                        {
-                            ForegroundColor = ConsoleColor.Green;
-                        }
-
-                        WriteLine(
-                            $"\t버그명 : {_selectedBugs[i].BugName}\n\t타입 : {_selectedBugs[i].BugType} | 디버깅 난이드 : {_selectedBugs[i].BugDifficulty} | 디버깅 복잡도 : {_selectedBugs[i].BugComplexity} | 디버깅 진행률 : {_selectedBugs[i].BugProgress}\n");
-                        ResetColor();
-                    }
+                    ForegroundColor = ConsoleColor.Green;
                 }
+                WriteLine(
+                    $"\t버그명 : {selectedBugs[0].BugName}" +
+                    $"\n\t버그 설명 : {selectedBugs[0].BugDesc}" +
+                    $"\n\t디버깅 난이도 : {selectedBugs[0].BugDifficulty} | 디버깅 복잡도 : {selectedBugs[0].BugComplexity} | 디버깅 진행률 : {selectedBugs[0].BugProgress} %\n");
+                ResetColor();
 
-                if (selectedIndex == _selectedBugs.Count)
+                if (choiceOption == 1)
                 {
                     ForegroundColor = ConsoleColor.Red;
                 }
-                WriteLine("\n\t컴파일 종료"); 
+                WriteLine("\t컴파일 종료"); 
                 ResetColor();
-
+                ConsoleSizeUtility.RedrawBorder();
                 ConsoleKeyInfo  key = ReadKey();
 
                 switch (key.Key)
                 {
                     case ConsoleKey.UpArrow:
-                        selectedIndex = (selectedIndex - 1 + totalOption) % totalOption;
-                        break;
                     case ConsoleKey.DownArrow:
-                        selectedIndex = (selectedIndex + 1) % totalOption;
+                        choiceOption = 1 - choiceOption;
                         break;
                     case ConsoleKey.Enter:
-                        return selectedIndex == _selectedBugs.Count ? null : _selectedBugs[selectedIndex];
+                        return choiceOption == 0 ? selectedBugs : null;
                 }
             }
         }
         # endregion
 
-        #region EscapeAction
+         #region Escape Action
         /// <summary>
         ///  전투 도중 "컴파일 종료 선택 시 탈출 안내창 출력"
         /// </summary>
@@ -208,13 +251,32 @@ namespace HexaCoreVillage.Dungeon
         }
         #endregion
 
-        #region Battle
-        private static void Debugging(Bug selectBug)
+        #region Battle Action
+
+        private static bool Debugging(List<Bug> selectBugs)
         {
+            int consoleWidth = WindowWidth;
+            int splitPosition = consoleWidth / 2; // Determine the split position
 
+            for (int i = 0; i < WindowHeight; i++)
+            {
+                SetCursorPosition(splitPosition, i);
+                Write("|"); // Drawing a dividing line
+            }
+
+            // Example of writing to the left side
+            SetCursorPosition(0, 0); // Set cursor to top left
+            WriteLine("Left side of the split");
+
+            // Example of writing to the right side
+            SetCursorPosition(splitPosition + 1, 0); // Set cursor to top right
+            WriteLine("Right side of the split");
+
+            ConsoleSizeUtility.RedrawBorder();
+            // Prevent console from closing immediately
+            ReadLine();
+            return true;
         }
-        
-
         #endregion
     }
 }
--- TeamDungeon/HexaCoreVillage/Dungeon/Reward.cs
+++ TeamDungeon/HexaCoreVillage/Dungeon/Reward.cs
@@ -14,4 +14,8 @@ public class Reward : Scene
     {
         throw new NotImplementedException();
     }
+
+    // Battle 성공시 Data Class의 "bool BattleSuccess" 로  bool 값 넣어두도록 하겠습니다,
+    // 해당 bool 값으로 성공 실패 정하시면 될 것 같아요 
+    // Battle에서 끝나면 Start() 호출해서 실행하도록 하겠습니다.
 }
