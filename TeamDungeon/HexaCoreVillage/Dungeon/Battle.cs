830230fd57e3acd72d6273b354011ab0553ea90e
Daniel Lee <jhoon8903@gmail.com>
2023-11-19 15:19:29 +0900

    Revert "[Feature] - Battle (Debugging) - Select Solutions"
    This reverts commit e6150d9ef6a691ff87643057f96ae921ad44bd1b.

--- TeamDungeon/HexaCoreVillage/Dungeon/Battle.cs
+++ TeamDungeon/HexaCoreVillage/Dungeon/Battle.cs
@@ -1,9 +1,6 @@
 using HexaCoreVillage.Utility;
 using Newtonsoft.Json;
 using System.Text;
-using static HexaCoreVillage.Dungeon.Debugging;
-using static HexaCoreVillage.Utility.ConsoleSizeUtility;
-using static HexaCoreVillage.Utility.DividerLineUtility;
 
 namespace HexaCoreVillage.Dungeon
 {
@@ -20,10 +17,6 @@ namespace HexaCoreVillage.Dungeon
         private static List<Bug>? _bugList = new List<Bug>();
         private static List<Bug>? _selectedBugs;
         private static readonly Random Random = new Random();
-        private static SolutionTypes _solution;
-        private static int _currentBugIndex = 0;
-        private static List<Bug> _debuggingBugs = new List<Bug>();
-        private static Bug _currentBug = new Bug();
 
         # region Start
         /// <summary>
@@ -41,11 +34,10 @@ namespace HexaCoreVillage.Dungeon
         public override void Update()
         {
             Compiling();
-           DebuggingList();
-            if (_debuggingBugs != null)
+            List<Bug> selectBugs = DebuggingList();
+            if (selectBugs != null)
             {
-                Data.BattleSuccess = Debugging();
-                // Reward 호출
+                Data.BattleSuccess = Debugging(selectBugs);
             }
             else
             {
@@ -80,22 +72,20 @@ namespace HexaCoreVillage.Dungeon
             CursorVisible = false;
             while (true)
             {
-
                 // "....."이 10초 단위로 Loop 하면서 초기화 진행
                 for (int i = 0; i < 10; i++)
                 {
                     Clear();
-                    WriteLine("\n\n\t[ Running To Project ]\n");
-
+                    WriteLine("\n\n\t[Running To Project]\n");
                     // 초당 1개씩 Dot을 추가
                     Write("\tCompiling" + new string('.', i));
-        
+                    
                     if (Random.Next(0,100) <=  Player.BugPercentage)
                     {
                         FoundBugs();
                         return;
-                    }
-                    RedrawBorder();
+                    }    
+                    ConsoleSizeUtility.RedrawBorder();
                     Thread.Sleep(1000);
                 }
             }
@@ -107,8 +97,9 @@ namespace HexaCoreVillage.Dungeon
         /// </summary>
         private static void FoundBugs()
         {
+            int bugsCount = 0;
             _selectedBugs = new List<Bug>();
-            int bugsCount = Player.Level switch
+            bugsCount = Player.Level switch
             {
                 1 => Random.Next(1, 4),
                 2 =>  Random.Next(1, 5),
@@ -143,7 +134,7 @@ namespace HexaCoreVillage.Dungeon
         ///  Console에 FoundBugs 중에서 고른 Bug 리스트의 첫 번재 버그를 출력
         /// </summary>
         /// <returns>선택한 Bug 리스트를 반환</returns>
-        private static void DebuggingList()
+        private static List<Bug> DebuggingList()
         {
             CursorVisible = false;
             Bug selectedBug = _selectedBugs[Random.Next(_selectedBugs.Count)];
@@ -155,8 +146,8 @@ namespace HexaCoreVillage.Dungeon
             while (true)
             {
                 Clear();
-                WriteLine("\n\n\t[ 디버깅 - 버그 확인 ]\n");
-                WriteLine($"\t[ {Player.NickName}의 상태 ]"); 
+                WriteLine("\n\n\t[디버깅 - 버그 확인]\n");
+                WriteLine($"\t[{Player.NickName}의 상태]"); 
                 WriteLine($"\t레벨 : {Player.Level}");
                 WriteLine($"\t체력 : {Player.CurrentHp} / {Player.HP}"); 
                 WriteLine($"\t디버깅 능력 : {Player.TypingSpeed}");
@@ -164,12 +155,12 @@ namespace HexaCoreVillage.Dungeon
                 Write("\t");
                 BackgroundColor = ConsoleColor.Red;
                 ForegroundColor = ConsoleColor.Black;
-                Write("[ Compile Error!! ]\n");
+                Write($"[Compile Error!!]\n");
                 ResetColor();
                 Write("\t");
                 BackgroundColor = ConsoleColor.Red;
                 ForegroundColor = ConsoleColor.Black;
-                Write($"[ {selectedBugs.Count}개의 버그가 확인 되었습니다! - 확인 된 버그는 다음과 같습니다. ]\n\n");
+                Write($"[{selectedBugs.Count}개의 버그가 확인 되었습니다! - 확인 된 버그는 다음과 같습니다.]\n\n");
                 ResetColor();
 
                 if (choiceOption == 0)
@@ -188,7 +179,7 @@ namespace HexaCoreVillage.Dungeon
                 }
                 WriteLine("\t컴파일 종료"); 
                 ResetColor();
-                RedrawBorder();
+                ConsoleSizeUtility.RedrawBorder();
                 ConsoleKeyInfo  key = ReadKey();
 
                 switch (key.Key)
@@ -198,8 +189,7 @@ namespace HexaCoreVillage.Dungeon
                         choiceOption = 1 - choiceOption;
                         break;
                     case ConsoleKey.Enter:
-                        _debuggingBugs = choiceOption == 0 ? selectedBugs : null;
-                        return;
+                        return choiceOption == 0 ? selectedBugs : null;
                 }
             }
         }
@@ -216,14 +206,14 @@ namespace HexaCoreVillage.Dungeon
             int totalOption = 2;
 
             while (true)
-            {
+            {           
                 Clear();
                 BackgroundColor = ConsoleColor.Yellow;
                 ForegroundColor = ConsoleColor.Black;
-                WriteLine("[ Warning ]");
+                WriteLine("[Warning]");
                 ResetColor();
-                WriteLine("\n[ 이대로 나가시면 스테미나가 감소합니다. ]\n");
-                WriteLine("[ 정말 컴파일을 종료하시겠습니까? ]");
+                WriteLine("\n[이대로 나가시면 스테미나가 감소합니다.]\n");
+                WriteLine("[정말 컴파일을 종료하시겠습니까?]");
 
                 string[] options = { "디버깅 돌아가기", "메인화면으로 돌아가기" };
                 for (int i = 0; i < totalOption; i++)
@@ -235,7 +225,7 @@ namespace HexaCoreVillage.Dungeon
                     WriteLine($"\n{options[i]}");
                     ResetColor();
                 }
-                RedrawBorder();
+
                 ConsoleKeyInfo key = ReadKey();
 
                 switch (key.Key)
@@ -262,254 +252,30 @@ namespace HexaCoreVillage.Dungeon
         #endregion
 
         #region Battle Action
-        private static bool Debugging()
+
+        private static bool Debugging(List<Bug> selectBugs)
         {
-            Clear();
-            const int splitPosition = FixedColumns / 4; // 분할 위치 결정
-            for (int i = 0; i < FixedRows; i++) // 분할선 그리기
+            int consoleWidth = WindowWidth;
+            int splitPosition = consoleWidth / 2; // Determine the split position
+
+            for (int i = 0; i < WindowHeight; i++)
             {
                 SetCursorPosition(splitPosition, i);
-                BackgroundColor = ConsoleColor.Yellow;
-                Write(" "); // 분할선 그리기
-                ResetColor();
+                Write("|"); // Drawing a dividing line
             }
 
-            // 왼쪽 영역에 텍스트 출력
-            RightWindow();
-            LeftWindow();
-            RedrawBorder();
-            ReadLine(); // 콘솔 창이 즉시 닫히는 것을 방지
+            // Example of writing to the left side
+            SetCursorPosition(0, 0); // Set cursor to top left
+            WriteLine("Left side of the split");
+
+            // Example of writing to the right side
+            SetCursorPosition(splitPosition + 1, 0); // Set cursor to top right
+            WriteLine("Right side of the split");
+
+            ConsoleSizeUtility.RedrawBorder();
+            // Prevent console from closing immediately
+            ReadLine();
             return true;
-        }
-        #endregion
-
-        #region LEFT WINDOWS
-        private static void LeftWindow()
-        {
-            // Left Top
-            PlayerInformation();
-            // Left Middle
-            SelectToDebugging();
-        }
-        private static void PlayerInformation()
-        {
-                SetCursorPosition(4, 1);
-                BackgroundColor = ConsoleColor.Green;
-                ForegroundColor = ConsoleColor.Black;
-                Write("[ Running To Debug ]");
-
-                SetCursorPosition(4, CursorTop + 2);
-                ResetColor();
-                Write($"[ {Player.NickName} 상태 ]");
-
-                SetCursorPosition(4, CursorTop + 2);
-                Write($"레벨 : {Player.Level}");
-
-                SetCursorPosition(4, CursorTop + 1);
-                Write($"체력 : {Player.CurrentHp} / {Player.HP}");
-
-                SetCursorPosition(4, CursorTop + 1);
-                Write($"디버깅 능력 : {Player.TypingSpeed}");
-
-                SetCursorPosition(4, CursorTop + 1);
-                Write($"버그 대응능력 : {Player.C}");
-
-                DividerLine(0,CursorTop + 2, ConsoleColor.Yellow);
-        }
-        private static void SelectToDebugging()
-        {
-            CursorVisible = false;
-            int debuggingOption = 0;
-            string[] options = Enum.GetNames(typeof(SolutionTypes));
-
-            while (true)
-            {
-                SetCursorPosition(4,  12);
-                BackgroundColor = ConsoleColor.Green;
-                ForegroundColor = ConsoleColor.Black;
-                Write("[ 디버깅 솔루션을 선택하세요 ]");
-                ResetColor();
-                for (int i = 0; i < options.Length; i++)
-                {
-                    SetCursorPosition(4, 14 + i);
-                    if (i == debuggingOption)
-                    {
-                        BackgroundColor = ConsoleColor.Green;
-                        ForegroundColor = ConsoleColor.Black;
-                    }
-                    else
-                    {
-                        ResetColor();
-                    }
-                    Write(options[i]);
-                    ResetColor();
-                }
-                RedrawBorder();
-                DividerLine(0,20, ConsoleColor.Yellow);
-                SolutionTypes solution = (SolutionTypes)Enum.Parse(typeof(SolutionTypes), options[debuggingOption]);
-                DescriptionSolution(solution);
-                ConsoleKeyInfo key = ReadKey();
-
-                switch (key.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        debuggingOption = (debuggingOption - 1 + options.Length) % options.Length; 
-                        DescriptionSolution(solution);
-                        break;
-                    case ConsoleKey.DownArrow:
-                        debuggingOption = (debuggingOption + 1) % options.Length;
-                        DescriptionSolution(solution);
-                        break;
-                    case ConsoleKey.Enter:
-                        _solution = solution;
-                        InputSolution();
-                        if (Data.BattleSuccess)
-                        {
-                            return;
-                        }
-                        break;
-                }
-            }
-        }
-        private static void DescriptionSolution(SolutionTypes solution)
-        {
-            SetCursorPosition(4, 22);
-            WriteLine(new string(' ', 40));
-            SetCursorPosition(4, 22);
-            Write($"[ {solution.ToString().ToUpper()} 솔루션 설명 ]\n\n");
-
-            switch (solution)
-            {
-                case SolutionTypes.ModifyVariables:
-                    WriteDesc("프로그램 실행 중 변수값", 24);
-                    WriteDesc("변경 사항 반영", 25);
-                    WriteDesc("사용자 입력 기반 업데이트", 26);
-                    WriteDesc("변수값 수정으로 오류 해결", 27);
-                    break;
-                case SolutionTypes.FixTypos:
-                    WriteDesc("코드 내 오타 수정 필요", 24);
-                    WriteDesc("변수명, 함수명 오류 검사", 25);
-                    WriteDesc("정확한 코드 작성 중요", 26);
-                    WriteDesc("예기치 못한 버그 예방", 27);
-                    break;
-                case SolutionTypes.ModifyScope:
-                    WriteDesc("변수 및 함수 범위 조정", 24);
-                    WriteDesc("코드 가독성 향상", 25);
-                    WriteDesc("적절한 스코프 설정", 26);
-                    WriteDesc("프로그램 안정성 향상", 27);
-                    break;
-                case SolutionTypes.EditPath:
-                    WriteDesc("파일/디렉토리 경로 수정", 24);
-                    WriteDesc("정확한 파일 접근 중요", 25);
-                    WriteDesc("경로 오류 해결 필요", 26);
-                    WriteDesc("프로그램 실행 보장", 27);
-                    break;
-                case SolutionTypes.CheckMemory:
-                    WriteDesc("메모리 사용 상태 점검", 24);
-                    WriteDesc("메모리 누수 탐지 및 해결", 25);
-                    WriteDesc("성능 및 안정성 최적화", 26);
-                    WriteDesc("효율적 메모리 관리", 27);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(solution), solution, null);
-            }
-        }
-        private static void WriteDesc(string desc, int cursorTop)
-        {
-            SetCursorPosition(4, cursorTop);
-            Write(new string(' ', 40));
-            SetCursorPosition(4, cursorTop);
-            Write(desc); 
-            DividerLine(1,29,ConsoleColor.Yellow); 
-            Logo();
-        }
-        private static void Logo()
-        {
-            string[] asciiArt = {
-                "██   ██ ███████ ██   ██  █████",  
-                "██   ██ ██       ██ ██  ██   ██", 
-                "███████ █████     ███   ███████", 
-                "██   ██ ██       ██ ██  ██   ██", 
-                "██   ██ ███████ ██   ██ ██   ██"
-            };
-
-            ConsoleColor[] colors = {
-                ConsoleColor.Red,
-                ConsoleColor.Yellow,
-                ConsoleColor.Green,
-                ConsoleColor.Cyan,
-                ConsoleColor.Blue,
-                ConsoleColor.Magenta
-            };
-
-            for (int i = 0; i < asciiArt.Length; i++)
-            {
-                SetCursorPosition(6, 32 + i);
-                ForegroundColor = colors[i % colors.Length];
-                WriteLine(asciiArt[i]);
-            }
-
-            ResetColor();
-        }
-        #endregion
-
-        #region RIGHT WINDOWS
-        private static void RightWindow()
-        {
-            BugStatus();
-        }
-
-        private static void BugStatus()
-        {
-            if (_currentBugIndex >= _debuggingBugs.Count)
-            {
-                WriteLine("디버그를 완료 하였습니다."); 
-                return;
-            }
-            _currentBug = _debuggingBugs[_currentBugIndex];
-            SetCursorPosition(50,2);
-            Write($"[ Bug : {_currentBug.BugName} ]");
-            SetCursorPosition(50,3);
-            Write($"[ Desc : {_currentBug.BugDesc} ]");
-            SetCursorPosition(50,4);
-            Write($"[ Debug Difficulty : {_currentBug.BugDifficulty}]");
-            SetCursorPosition(50,5);
-            Write($"[ Debug Complexity : {_currentBug.BugComplexity}]");
-            SetCursorPosition(50,6);
-            Write($"[ Debug Process : {_currentBug.BugProgress} % ]  ");
-            const int totalBlocks = 100;
-            int greenBlocks = _currentBug.BugProgress;
-
-            ForegroundColor = ConsoleColor.Green;
-            for (int i = 0; i < greenBlocks; i++)
-            {
-                Write('█');
-            }
-            ForegroundColor = ConsoleColor.DarkRed;
-            for (int i = greenBlocks; i < totalBlocks; i++)
-            {
-                Write('█');
-            }
-            ResetColor();
-        }
-
-        private static void InputSolution()
-        {
-            SetCursorPosition(50, 10);
-            if (_currentBug.SolutionType == _solution)
-            {
-                _currentBug.BugProgress += Random.Next(0,20);
-            }
-            else
-            {
-                _currentBug.BugProgress += Random.Next(0,10);
-            }
-            if (_currentBug.BugProgress > 100)
-            {
-                _currentBug.BugProgress = 100;
-                _currentBugIndex++;
-            }
-            BugStatus();
         }
         #endregion
     }
