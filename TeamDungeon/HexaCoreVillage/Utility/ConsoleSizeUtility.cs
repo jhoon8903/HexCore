afcc14d6f0cc3fd9011566d30241f8cd047c9066
Daniel Lee <jhoon8903@gmail.com>
2023-11-20 10:41:53 +0900

    [👨🏻‍🚒 HOT FIX] - Console Fixer Hot FIX
    - 콘솔 유틸리티 Wndows 호환 불가 현상 조치
    - Unix 계열 및 Window 계열에서도 사용 가능한 dll 추가
    - OS Platform 을 시스템에서 확이하여 조건문으로 각 실행하는 DLL을 바꿈

--- TeamDungeon/HexaCoreVillage/Utility/ConsoleSizeUtility.cs
+++ TeamDungeon/HexaCoreVillage/Utility/ConsoleSizeUtility.cs
@@ -3,15 +3,46 @@ namespace HexaCoreVillage.Utility
 {
     public static class ConsoleSizeUtility
     {
-        [DllImport ("libc")] private static extern int system (string exec);
+        // For Unix systems
+        [DllImport("libc")]
+        private static extern int system(string exec);
+
+        // For Windows systems
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         public const int FixedRows = 40;
         public const int FixedColumns = 180;
 
-
-        public static void FixConsole()
+        private static void FixConsole()
         {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                FixConsoleWindows();
+            }
+            else
+            {
+                FixConsoleMac();
+            }
+        }
+
+        private static void FixConsoleWindows()
+        {
+            // Adjust the window size for Windows
+            IntPtr consoleWindow = GetConsoleWindow();
+            // 3 is the SW_MAXIMIZE value to maximize the console window
+            ShowWindow(consoleWindow, 3);
+        }
+
+        private static void FixConsoleMac()
+        {
+            // Adjust the window size for Unix-like systems
             system($@"printf '\e[8;{FixedRows};{FixedColumns}t'");
         }
+
+
         public static void RedrawBorder()
         {
             ForegroundColor = ConsoleColor.Green;
