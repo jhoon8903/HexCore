05e12d020b2242741a426367676f0c1b7456237a
I'm Developer? <59020441+iamdeveloperz@users.noreply.github.com>
2023-11-22 19:13:19 +0900

    [➕Add] : Data - SavePlayerData Function

--- TeamDungeon/HexaCoreVillage/Utility/Data.cs
+++ TeamDungeon/HexaCoreVillage/Utility/Data.cs
@@ -1,4 +1,6 @@
 
+using Newtonsoft.Json;
+
 namespace HexaCoreVillage.Utility;
 
 public static class Data
@@ -7,4 +9,12 @@ public static class Data
     public static bool BattleSuccess { get; set; }
     public static int BugCount { get; set; }
     #endregion
+
+    public static void SavePlayerData(Player player)
+    {
+        // 경로 지정 (리소스 폴더와 json파일의 이름을 합침)
+        var playerDataPath = Path.Combine(Managers.Resource.GetResourceFolderPath(), Literals.PlayerDataPath);
+        string playerData = JsonConvert.SerializeObject(player, Formatting.Indented);        //캐릭터 정보 저장
+        File.WriteAllText(playerDataPath, playerData);
+    }
 }
